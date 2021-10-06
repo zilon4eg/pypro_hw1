66f1bff55026ead69d5598933193a6149b06585a
Igor <hdd500t@yandex.ru>
2021-10-06 22:34:39 +0300

    complete

--- dirty_main.py
+++ dirty_main.py
@@ -1,2 +1,8 @@
-from package import *
+from application import *
+from main import *
 
+if __name__ == '__main__':
+    print(f'Модуль: dirty_main.py')
+    print(f'Текущая дата/время: {datetime.now()}')
+    print(f'Функция из модуля: {print_people()}')
+    print(f'Функция из модуля: {print_salary()}')
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from datetime import datetime
 from application.db.people import print_people
 from application.salary import print_salary
 
+def print_main():
+    print('Это функция скрита из main.py')
+
 if __name__ == '__main__':
     print(f'Текущая дата/время: {datetime.now()}')
     print(f'Функция из модуля: {print_people()}')
     print(f'Функция из модуля: {print_salary()}')
+    print_main()
